77a90a74b2b224fcc68db0081264258ea6e2ffa8
sarahturlington <sarahturlington@utexas.edu>
2018-04-29 17:29:13 -0500

    graph changes

--- app/src/main/java/com/example/sarah/baait_version1/TabTwo.java
+++ app/src/main/java/com/example/sarah/baait_version1/TabTwo.java
@@ -19,6 +19,7 @@ public class TabTwo extends Fragment implements graphControl
 
 
 {
+    public GraphView graph;
     public LineGraphSeries<DataPoint> series = null;
     int maxlength = 30;
     long x_value = 0;
@@ -27,7 +28,7 @@ public class TabTwo extends Fragment implements graphControl
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_two, container, false);
-        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        graph = (GraphView) view.findViewById(R.id.graph);
         series = new LineGraphSeries<DataPoint>();
         series.appendData(new DataPoint(0, 98.6),true,maxlength);
         series.appendData(new DataPoint(1,98.1), true, maxlength);
@@ -51,7 +52,9 @@ public class TabTwo extends Fragment implements graphControl
     public boolean addToGraph(int y){
         series.appendData(new DataPoint(x_value,y),true, maxlength);
         x_value++;
+        graph.addSeries(series);
         return true;
+
     }
 
 
