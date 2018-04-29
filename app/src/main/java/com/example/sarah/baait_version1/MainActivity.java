7d20c326181a24732654a552edaad84c0c0eacf8
HaydenBrewBrew <haydenlydick@utexas.edu>
2018-04-29 17:01:15 -0500

    missed some

--- app/src/main/java/com/example/sarah/baait_version1/MainActivity.java
+++ app/src/main/java/com/example/sarah/baait_version1/MainActivity.java
@@ -74,9 +74,7 @@ public class MainActivity extends AppCompatActivity {
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        try{openBT();}
-        catch(Exception e){
-        }
+        
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
